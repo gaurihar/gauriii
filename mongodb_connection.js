007012d10778ff6d7452a4a954cf883959b152fb
gaurihar <gaurihardarade@gmail.com>
2019-06-17 16:23:06 +0530

    insert query on mongodb using nodejs

--- mongodb_connection.js
+++ mongodb_connection.js
@@ -3,6 +3,6 @@ var url = "mongodb://localhost:27017/student";
 
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
   if (err) throw err;
-  console.log("Database created succesfully!!!!!");
+  console.log("Database created !!!!!");
   db.close();
 });
